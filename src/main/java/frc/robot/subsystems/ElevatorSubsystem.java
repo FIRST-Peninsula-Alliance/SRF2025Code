f09b21113de13056914e78a56f0e53d4a1ed57a7
SrfGoinf <gavin.mayfield225@gmail.com>
2025-02-12 19:22:17 -0800

    recreated elevator cancoder

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,25 +5,31 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.ClosedLoopRampsConfigs;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CAC;
 import frc.robot.Constants.EC;
 //import frc.robot.NotableConstants.IAC;
 
 public class ElevatorSubsystem extends SubsystemBase {
   public final TalonFX m_elevatorMotor; 
   private double m_elevatorSetpoint;
+  private final CANcoder m_elevatorCANCoder;
+  private final double m_magnetOffset = EC.ELEVATOR_CANCODER_MAGNET_OFFSET;
   private final MotionMagicVoltage m_elevatorMagicCtrl = new MotionMagicVoltage(0.0)
                                                                                 .withSlot(0)
                                                                                 .withEnableFOC(true);
@@ -31,13 +37,30 @@ public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem() {
     m_elevatorMotor = new TalonFX(EC.ELEVATOR_MOTOR_CAN_ID);
+    m_elevatorCANCoder = new CANcoder(EC.ELEVATOR_CANCODER_ID);
 
+    configElevatorCANCoder();
     configElevatorMotor();
   }
 
   public void GoToPosition(double position) {
     m_elevatorSetpoint = position;
     m_elevatorMotor.setControl(m_elevatorMagicCtrl.withPosition(m_elevatorSetpoint));
+  }
+
+  public double getAbsCoralArmPos() {
+    return(m_elevatorCANCoder.getAbsolutePosition().getValueAsDouble());
+  }
+
+  private void configElevatorCANCoder() {
+    var magnetSensorConfigs = new MagnetSensorConfigs().withAbsoluteSensorDiscontinuityPoint(CAC.CORAL_ARM_CANCODER_RANGE)
+                                                       .withSensorDirection(CAC.CORAL_ARM_CANCODER_DIR)
+                                                       .withMagnetOffset(m_magnetOffset);
+    var ccConfig = new CANcoderConfiguration().withMagnetSensor(magnetSensorConfigs);
+    StatusCode status = m_elevatorCANCoder.getConfigurator().apply(ccConfig);
+    if (! status.isOK() ) {
+      System.out.println("Failed to apply CORAL_CANcoder configs. Error code: "+status.toString());
+    }
   }
 
   private void configElevatorMotor() {
@@ -81,8 +104,7 @@ public class ElevatorSubsystem extends SubsystemBase {
       System.out.println("Failed to apply ELEVATOR configs. Error code: "+status.toString());
     }
   }
-
-
+  
   public void GoToCoralPickupPosition() {
     GoToPosition(EC.ELEVATOR_SOURCE_POSITION);
   }
